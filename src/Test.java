8cd018cc41cbd3aa9ac816328d08129a34f2e68e
rT_Troy <yotroy@foxmail.com>
2022-08-12 20:25:33 +0100

    update comments

--- src/Test.java
+++ src/Test.java
@@ -2,37 +2,36 @@ import java.sql.*;
 
 public class Test {
 
-    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/web";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";//驱动类名
+    static final String DB_URL = "jdbc:mysql://localhost:3306";//数据库URL地址
 
-    static final String USERNAME = "user";
-    static final String PASSWORD = "123456";
+    static final String USERNAME = "user";//数据库用户名
+    static final String PASSWORD = "123456";//数据库密码
 
     public static void main(String[] args){
 
         Connection conn = null;
         Statement stat = null;
+
         try {
             Class.forName(JDBC_DRIVER);
             System.out.println("Connecting...");
             conn = DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
             stat = conn.createStatement();
 
-            String sql = "select * from web.students ";
+            String sql = "SELECT * FROM test.students";//此处为MySQL语句
             ResultSet rs = stat.executeQuery(sql);
 
-            while(rs.next()){
+            while(rs.next()){//遍历
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 int age = rs.getInt("age");
-                String grade = rs.getString("grade");
                 System.out.println("id = " + id);
                 System.out.println("name = " + name);
                 System.out.println("age = " + age);
-                System.out.println("grade = " + grade);
             }
 
-            rs.close();
+            rs.close();//释放
             stat.close();
             conn.close();
 
